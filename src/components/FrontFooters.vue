eb0f596eb8dc1afdef1f9a3f30a5436bbfa6a6f0
Kevin Chang <apog780212@gmail.com>
2022-03-28 22:02:37 +0800

    <20220328_第一次上傳>

--- src/components/FrontFooters.vue
+++ src/components/FrontFooters.vue
@@ -2,34 +2,34 @@
   <div class="container-fluid footer bg-warning py-2 py-md-5">
     <div class="container">
       <div class="row footer-list">
-        <ul class="col-md-6 footer-contact">
-            <li>
-              <a href="mailto:camera@business.com" class="fs-3 text-secondary ms-3">
-                <i class="bi bi-envelope fs-3 text-secondary"></i>
-                camera@shop-shutter.com
-              </a>
-            </li>
-            <li>
-              <a href="tel:0800-000-000" class="fs-3 text-secondary ms-3">
-                <i class="bi bi-telephone fs-3 text-secondary"></i>
-                0800-123-456
-              </a>
-            </li>
+        <ul class="col-md-8 footer-contact">
+          <li>
+            <a href="mailto:camera@business.com" class="fs-3 text-secondary ms-3">
+              <i class="bi bi-envelope fs-3 text-secondary"></i>
+              camera@shop-shutter.com
+            </a>
+          </li>
+          <li>
+            <a href="tel:0800-000-000" class="fs-3 text-secondary ms-3">
+              <i class="bi bi-telephone fs-3 text-secondary"></i>
+              0800-123-456
+            </a>
+          </li>
         </ul>
-        <ul class="col-md-6 footer-icon d-flex justify-content-start justify-content-md-end">
+        <ul class="col-md-4 footer-icon d-flex justify-content-start justify-content-md-end">
           <li class="me-3 me-md-8">
             <a href="https://www.facebook.com/profile.php?id=1844418885" target="_blank">
-              <img src="../assets/images/icon-fb.svg" alt="fb-icon">
+              <img src="../assets/images/icon-fb.svg" alt="fb-icon" />
             </a>
           </li>
           <li class="me-3 me-md-8">
             <a href="https://www.instagram.com/kevin780212/" target="_blank">
-              <img src="../assets/images/icon-ig.svg" alt="ig-icon">
+              <img src="../assets/images/icon-ig.svg" alt="ig-icon" />
             </a>
           </li>
           <li class="">
             <a href="#">
-              <img src="../assets/images/icon-line.svg" alt="line-icon" target="_blank">
+              <img src="../assets/images/icon-line.svg" alt="line-icon" target="_blank" />
             </a>
           </li>
         </ul>
